841d1513c9e4778bbc7938447141d7b0fb7a68f2
Bryan Yong <byon0001@student.monash.edu>
2021-03-01 12:23:29 +0800

    Updated API caller dart file

--- lib/functional_class/api_caller.dart
+++ lib/functional_class/api_caller.dart
@@ -1,3 +1,8 @@
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+
 class ApiCaller {
   static const BASEURL = 'https://bestweb.my/interview_test/';
   static const APIKEY = 'interviewtest@8888';
@@ -5,24 +10,37 @@ class ApiCaller {
     "Content-Type": "application/x-www-form-urlencoded",
   };
 
+
   ///TODO: Create your api caller functions here
   // Below are sample functions to make GET and POST API calls, you can
   // write your own function too :)
   //
-  // static Future getFunction() async {
-  //   var response = http.get(<api_url>, headers: <headers>);
-  //   var data = var data = jsonDecode(response.body);
-  //   return data;
-  // }
-  //
-  // static Future postFunction(variable) async {
-  //   var response = await http.post(<api_url>,
-  //       headers: <headers>,
-  //       body: {
-  //         'param': variable,
-  //       });
-  //   var data = json.decode(response.body);
-  //   return data;
-  // }
+   static Future getFunction() async {
+     var response = await http.get("https://www.getpostman.com/collections/4d746faa4c27b666871e", headers: API_DEFAULT_REQUEST_HEADERS);
+
+     if (response.statusCode == 200){
+       var data = jsonDecode(response.body);
+       return data;
+     } else {
+       print(response.statusCode);
+       return ; }
+
+
+   }
+
+   static Future postFunction(variable) async {
+     var response = await http.post("https://www.getpostman.com/collections/4d746faa4c27b666871e",
+         headers: API_DEFAULT_REQUEST_HEADERS ,
+         body: {
+           'param': variable,
+         });
+
+     if (response.statusCode == 200){
+       var data = jsonDecode(response.body);
+       return data;
+     } else {
+       print(response.statusCode);
+       return ; }
+   }
 
 }
